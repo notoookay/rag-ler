de2a4686090fc55cfc39c50fa07899b782144d6c
wenyang <yang.t.skywalker@gmail.com>
2024-12-31 10:55:27 +0800

    Update finetune_reranker script

--- scripts/finetune_reranker.sh
+++ scripts/finetune_reranker.sh
@@ -1,16 +1,10 @@
 export CUDA_VISIBLE_DEVICES=0,1
-# export MLFLOW_ENABLE_SYSTEM_METRICS_LOGGING=true
-# add this if mlflow raise the error:
-#   mlflow.exceptions.MlflowException: The configured tracking uri scheme: 'file' is invalid for u se with the proxy mlflow-artifact scheme. The allowed tracking schemes are: {'http', 'https'}
-# export MLFLOW_TRACKING_URI=http://localhost:5000
 
-MODEL=ms-marco-MiniLM-L-6-v2
-BASE_MODEL=cross-encoder/ms-marco-MiniLM-L-6-v2
+BASE_MODEL=cross-encoder/ms-marco-MiniLM-L-12-v2
 DATASET=reranker_train_labels_llama2_7b.jsonl
-TRAIN_METHOD=sft
 NUM_GPUS=2
 BATCH_SIZE_PER_GPU=1
-TOTAL_BATCH_SIZE=128
+TOTAL_BATCH_SIZE=64
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
 EXPERIMENT_NAME="Fine-tuning Re-ranker"
 echo "Training Re-ranker $BASE_MODEL using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
@@ -26,9 +20,10 @@ accelerate launch \
     --model_name_or_path ${BASE_MODEL} \
     --tokenizer_name ${BASE_MODEL} \
     --use_slow_tokenizer \
-    --train_file data/${DATASET} \
+    --train_file ${DATASET} \
     --max_seq_length 512 \
     --ctxs_num 30 \
+    --d_conf_threshold 0.8 \
     --preprocessing_num_workers 20 \
     --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
     --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
@@ -36,10 +31,10 @@ accelerate launch \
     --lr_scheduler_type linear \
     --warmup_ratio 0.03 \
     --weight_decay 0. \
-    --num_train_epochs 2 \
-    --output_dir output/model/${MODEL}_${DATASET}_${TRAIN_METHOD} \
+    --num_train_epochs 1 \
+    --output_dir output/model \
     --with_tracking \
-    --report_to mlflow \
+    --report_to wandb \
     --experiment_name "${EXPERIMENT_NAME}" \
-    --run_name "Fine-tuning ${MODEL} on ${DATASET} data with ${TRAIN_METHOD}" \
-    --logging_steps 1
+    --logging_steps 1 \
+    --checkpointing_steps epoch \
